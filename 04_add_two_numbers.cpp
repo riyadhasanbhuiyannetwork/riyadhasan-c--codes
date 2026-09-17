c87af48a6ce6d33a746b7d9bcbca6ae9e3483b59
Riyad Hasan Bhuiyan <riyadhasan4m1414@gmail.com>
2026-09-17 07:29:00 +0600

    Add files via upload

---
+++ 04_add_two_numbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double a, b;
+    cout << "Enter first number: ";
+    cin >> a;
+    cout << "Enter second number: ";
+    cin >> b;
+
+    double sum = a + b;
+    cout << "Sum = " << sum << endl;
+    return 0;
+}
