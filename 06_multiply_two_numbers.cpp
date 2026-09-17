23ab5bdaf2c30ac3ac8317bbf267a34fb544a9a7
Riyad Hasan Bhuiyan <riyadhasan4m1414@gmail.com>
2026-09-17 07:29:33 +0600

    Add files via upload

---
+++ 06_multiply_two_numbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double a, b;
+    cout << "Enter first number: ";
+    cin >> a;
+    cout << "Enter second number: ";
+    cin >> b;
+
+    double product = a * b;
+    cout << "Product = " << product << endl;
+    return 0;
+}
