dae6ae26b7e4ce66ddd8e6d31f900b2370d7a1f8
Riyad Hasan Bhuiyan <riyadhasan4m1414@gmail.com>
2026-09-17 07:29:19 +0600

    Add files via upload

---
+++ 05_subtract_two_numbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double a, b;
+    cout << "Enter first number: ";
+    cin >> a;
+    cout << "Enter second number: ";
+    cin >> b;
+
+    double difference = a - b;
+    cout << "Difference = " << difference << endl;
+    return 0;
+}
