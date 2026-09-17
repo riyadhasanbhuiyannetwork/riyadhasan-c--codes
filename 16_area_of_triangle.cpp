316622b10742c9a098fc814054c406f95b2ed3bc
Riyad Hasan Bhuiyan <riyadhasan4m1414@gmail.com>
2026-09-17 07:32:50 +0600

    Add files via upload

---
+++ 16_area_of_triangle.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double base, height;
+
+    cout << "Enter base of triangle: ";
+    cin >> base;
+    cout << "Enter height of triangle: ";
+    cin >> height;
+
+    double area = 0.5 * base * height;
+    cout << "Area of triangle = " << area << endl;
+    return 0;
+}
