71cfeb63cec3e0daa54f906fd12de29a302b25e1
Riyad Hasan Bhuiyan <riyadhasan4m1414@gmail.com>
2026-09-17 07:35:42 +0600

    Add files via upload

---
+++ 24_electricity_bill.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int units;
+    cout << "Enter number of units consumed: ";
+    cin >> units;
+
+    double bill = 0;
+
+    if (units <= 100) {
+        bill = units * 5.0;
+    } else if (units <= 200) {
+        bill = 100 * 5.0 + (units - 100) * 6.5;
+    } else {
+        bill = 100 * 5.0 + 100 * 6.5 + (units - 200) * 8.0;
+    }
+
+    cout << "Electricity Bill = " << bill << endl;
+    return 0;
+}
