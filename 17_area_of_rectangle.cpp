66284defe5820cc8aa75b905948e763284a4b856
Riyad Hasan Bhuiyan <riyadhasan4m1414@gmail.com>
2026-09-17 07:33:14 +0600

    Add files via upload

---
+++ 17_area_of_rectangle.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double length, width;
+
+    cout << "Enter length of rectangle: ";
+    cin >> length;
+    cout << "Enter width of rectangle: ";
+    cin >> width;
+
+    double area = length * width;
+    cout << "Area of rectangle = " << area << endl;
+    return 0;
+}
