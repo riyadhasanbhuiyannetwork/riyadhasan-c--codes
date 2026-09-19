b65e3df7151cd4817b0a83976a14e45116fa94fb
Riyad Hasan Bhuiyan <riyadhasan4m1414@gmail.com>
2026-09-19 15:54:43 +0600

    22_days_to_ymd.cpp

--- 22_days_to_ymd.cpp
+++ 22_days_to_ymd.cpp
@@ -3,18 +3,18 @@ using namespace std;
 
 int main() {
     int totalDays;
-    cout << "Enter total number of days: ";
+ cout << "Enter total number of days: ";
     cin >> totalDays;
 
-    int years = totalDays / 365;
+int years = totalDays / 365;
     int remainingAfterYears = totalDays % 365;
 
-    int months = remainingAfterYears / 30;
+int months = remainingAfterYears / 30;
     int days = remainingAfterYears % 30;
 
-    cout << totalDays << " days = "
+ cout << totalDays << " days = "
          << years << " years, "
-         << months << " months, "
-         << days << " days" << endl;
+   << months << " months, "
+       << days << " days" << endl;
     return 0;
 }
