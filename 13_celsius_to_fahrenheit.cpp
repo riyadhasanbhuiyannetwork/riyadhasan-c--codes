b7616f60fd40436266c4aaa83543a0151b41b7d0
Riyad Hasan Bhuiyan <riyadhasan4m1414@gmail.com>
2026-09-19 15:46:11 +0600

    13_celsius_to_fahrenheit.cpp

--- 13_celsius_to_fahrenheit.cpp
+++ 13_celsius_to_fahrenheit.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
 using namespace std;
-
-int main() {
-    double celsius;
-    cout << "Enter temperature in Celsius: ";
-    cin >> celsius;
-
-    double fahrenheit = (celsius * 9.0 / 5.0) + 32;
-    cout << celsius << " Celsius = " << fahrenheit << " Fahrenheit" << endl;
+int main()
+{
+    float c,f;
+    cin>>c;
+    f=(c*9/5)+32;
+    cout<< "Fahrenheit = " << f;
     return 0;
 }
