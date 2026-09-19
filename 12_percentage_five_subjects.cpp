4c67644913c7b8097d37efed0484a80bf072393c
Riyad Hasan Bhuiyan <riyadhasan4m1414@gmail.com>
2026-09-19 15:45:45 +0600

    12_percentage_five_subjects.cpp

--- 12_percentage_five_subjects.cpp
+++ 12_percentage_five_subjects.cpp
@@ -1,15 +1,11 @@
 #include <iostream>
 using namespace std;
-
-int main() {
-    double s1, s2, s3, s4, s5;
-    cout << "Enter marks for 5 subjects (out of 100 each): " << endl;
-    cin >> s1 >> s2 >> s3 >> s4 >> s5;
-
-    double total = s1 + s2 + s3 + s4 + s5;
-    double percentage = (total / 500.0) * 100;
-
-    cout << "Total marks = " << total << " out of 500" << endl;
-    cout << "Percentage = " << percentage << "%" << endl;
+int main()
+{
+    float a,b,c,d,e,total,percentage;
+    cin >>a>>b>>c>>d>>e;
+    total=a+b+c+d+e;
+    percentage=(total/500)*100;
+    cout<< "Percentage = " <<percentage<< "%";
     return 0;
 }
