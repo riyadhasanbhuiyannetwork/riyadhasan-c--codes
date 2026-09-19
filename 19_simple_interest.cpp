217bf1562d0a283e2728ad6d79b926587bf01a4c
Riyad Hasan Bhuiyan <riyadhasan4m1414@gmail.com>
2026-09-19 15:53:30 +0600

    19_simple_interest.cpp

--- 19_simple_interest.cpp
+++ 19_simple_interest.cpp
@@ -4,14 +4,14 @@ using namespace std;
 int main() {
     double principal, rate, time;
 
-    cout << "Enter principal amount: ";
-    cin >> principal;
+ cout << "Enter principal amount: ";
+  cin >> principal;
     cout << "Enter annual interest rate (%): ";
-    cin >> rate;
-    cout << "Enter time (in years): ";
+  cin >> rate;
+ cout << "Enter time (in years): ";
     cin >> time;
 
-    double simpleInterest = (principal * rate * time) / 100;
+ double simpleInterest = (principal * rate * time) / 100;
     cout << "Simple Interest = " << simpleInterest << endl;
     return 0;
 }
