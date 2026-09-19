f7631a3d15d247b7a30f708f53a2b5bb4dfc2c8a
Riyad Hasan Bhuiyan <riyadhasan4m1414@gmail.com>
2026-09-19 15:53:06 +0600

    18_perimeter_of_rectangle.cpp

--- 18_perimeter_of_rectangle.cpp
+++ 18_perimeter_of_rectangle.cpp
@@ -4,12 +4,12 @@ using namespace std;
 int main() {
     double length, width;
 
-    cout << "Enter length of rectangle: ";
+  cout << "Enter length of rectangle: ";
     cin >> length;
-    cout << "Enter width of rectangle: ";
-    cin >> width;
+ cout << "Enter width of rectangle: ";
+ cin >> width;
 
     double perimeter = 2 * (length + width);
-    cout << "Perimeter of rectangle = " << perimeter << endl;
+  cout << "Perimeter of rectangle = " << perimeter << endl;
     return 0;
 }
