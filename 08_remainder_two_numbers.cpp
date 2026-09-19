a793e3db81e9a13beb5b2f37fda8c47b6e00fe52
Riyad Hasan Bhuiyan <riyadhasan4m1414@gmail.com>
2026-09-19 15:43:00 +0600

    08_remainder_two_numbers.cpp

--- 08_remainder_two_numbers.cpp
+++ 08_remainder_two_numbers.cpp
@@ -1,17 +1,10 @@
 #include <iostream>
 using namespace std;
-
-int main() {
-    int a, b;
-    cout << "Enter first number: ";
-    cin >> a;
-    cout << "Enter second number: ";
-    cin >> b;
-
-    if (b == 0) {
-        cout << "Cannot divide by zero!" << endl;
-    } else {
-        cout << "Remainder = " << a % b << endl;
-    }
+int main()
+{
+    int a,b,remainder;
+    cin>>a>>b;
+    remainder=a%b;
+    cout<<"Remainder="<<remainder;
     return 0;
 }
