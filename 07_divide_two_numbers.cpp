4877cc06a2de03fb4f87ba64212964dbd19bc886
Riyad Hasan Bhuiyan <riyadhasan4m1414@gmail.com>
2026-09-19 15:42:25 +0600

    07_divide_two_numbers.cpp

--- 07_divide_two_numbers.cpp
+++ 07_divide_two_numbers.cpp
@@ -1,17 +1,10 @@
 #include <iostream>
 using namespace std;
-
-int main() {
-    double a, b;
-    cout << "Enter numerator: ";
-    cin >> a;
-    cout << "Enter denominator: ";
-    cin >> b;
-
-    if (b == 0) {
-        cout << "Cannot divide by zero!" << endl;
-    } else {
-        cout << "Quotient = " << a / b << endl;
-    }
+int main()
+{
+    int a, b, divide;
+    cin >> a >> b;
+    divide = a/b;
+    cout << "Divide = " << divide;
     return 0;
 }
