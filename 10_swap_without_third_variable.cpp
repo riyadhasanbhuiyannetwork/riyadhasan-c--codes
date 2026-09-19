5c2cc431fa33b931923551ef7c1d6ae602a1b634
Riyad Hasan Bhuiyan <riyadhasan4m1414@gmail.com>
2026-09-19 15:44:15 +0600

    10_swap_without_third_variable.cpp

--- 10_swap_without_third_variable.cpp
+++ 10_swap_without_third_variable.cpp
@@ -1,19 +1,12 @@
 #include <iostream>
 using namespace std;
-
-int main() {
-    int a, b;
-    cout << "Enter first number: ";
-    cin >> a;
-    cout << "Enter second number: ";
-    cin >> b;
-
-    cout << "Before swap: a = " << a << ", b = " << b << endl;
-
-    a = a + b;
-    b = a - b;
-    a = a - b;
-
-    cout << "After swap: a = " << a << ", b = " << b << endl;
+int main()
+{
+    int a,b;
+    cin >>a>>b;
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    cout <<"After swapping: a="<<a<<",b="<<b;
     return 0;
 }
