9b2913e25ecccce40da3991b88642c25f1c424f9
Riyad Hasan Bhuiyan <riyadhasan4m1414@gmail.com>
2026-09-19 15:55:10 +0600

    23_gross_salary.cpp

--- 23_gross_salary.cpp
+++ 23_gross_salary.cpp
@@ -3,16 +3,15 @@ using namespace std;
 
 int main() {
     double basicSalary;
-    cout << "Enter basic salary: ";
+ cout << "Enter basic salary: ";
     cin >> basicSalary;
 
-    double hra = 0.20 * basicSalary;
+  double hra = 0.20 * basicSalary;
     double da  = 0.80 * basicSalary;
-
     double grossSalary = basicSalary + hra + da;
 
-    cout << "HRA = " << hra << endl;
-    cout << "DA = " << da << endl;
-    cout << "Gross Salary = " << grossSalary << endl;
+  cout << "HRA = " << hra << endl;
+ cout << "DA = " << da << endl;
+ cout << "Gross Salary = " << grossSalary << endl;
     return 0;
 }
