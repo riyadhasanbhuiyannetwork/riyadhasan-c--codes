fdb0b9e74388906f5f31e70bc3a74b3d9cbb15ad
Riyad Hasan Bhuiyan <riyadhasan4m1414@gmail.com>
2026-09-19 15:54:20 +0600

    21_seconds_to_hms.cpp

--- 21_seconds_to_hms.cpp
+++ 21_seconds_to_hms.cpp
@@ -3,17 +3,17 @@ using namespace std;
 
 int main() {
     long totalSeconds;
-    cout << "Enter total seconds: ";
-    cin >> totalSeconds;
+ cout << "Enter total seconds: ";
+ cin >> totalSeconds;
 
-    long hours = totalSeconds / 3600;
-    long remaining = totalSeconds % 3600;
-    long minutes = remaining / 60;
-    long seconds = remaining % 60;
+  long hours = totalSeconds / 3600;
+  long remaining = totalSeconds % 3600;
+  long minutes = remaining / 60;
+ long seconds = remaining % 60;
 
     cout << totalSeconds << " seconds = "
-         << hours << " hours, "
-         << minutes << " minutes, "
-         << seconds << " seconds" << endl;
+   << hours << " hours, "
+        << minutes << " minutes, "
+      << seconds << " seconds" << endl;
     return 0;
 }
